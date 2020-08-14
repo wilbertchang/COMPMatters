52d3787f0500ad1fcf9c921bdc6447cfa6d3c2b0
mjhinek <jason.hinek@carleton.ca>
2020-08-14 01:52:36 -0400

    Create initials.py
    Starter code for the classes initials. 
    Draws a box and then resets the turtle.

---
+++ initials.py
@@ -0,0 +1,48 @@
+# initials program 
+from turtle import *
+
+# Do not change these lines (and do not create your own turtle... use t for your initials
+t = Turtle()
+t.speed(3)
+
+# first initials (well, it is a box...)
+t.up()
+t.pencolor('red')
+t.pensize(3)
+t.forward(200)
+t.left(90)
+t.down()
+t.forward(200)
+t.left(90)
+t.forward(400)
+t.left(90)
+t.forward(400)
+t.left(90)
+t.forward(400)
+t.left(90)
+t.forward(200)
+
+t.reset()
+
+
+# add your initials below and then call t.reset(), like above, so that 
+# the turtle goes back to the initial state for the next initials to state
+# (the pencolor and pensize are also reset).
+# draw your initials with a different pensize and pencolor
+
+
+
+
+
+
+
+
+
+
+
+# leave the input() function at the bottom of the code
+input('hit enter to exit')
+
+
+
+
